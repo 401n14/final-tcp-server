517b16b5bef8ca7e34c76777ad5517701f01b8e6
James Dunn <jamesdu@zillowgroup.com>
2020-02-17 15:57:01 -0800

    Removed firebase files and added socket events for user entering, leaving, and an event for displaying user group

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-const functions = require('firebase-functions');
 const server = require("socket.io")();
 
-server.on("connection", client => {
+const socketPool = {};
+
+server.on("connection", client => {  
+  
+  client.on('username', socket => {
+    console.log('SOCKET: ', socket)
+    socketPool[client.id] = socket.name;
+    // Send this information back to client, to render userlist
+    server.emit('broadcast', {
+      user: socket.name,
+      userGroup: socketPool
+    })
+  });
+
+  client.on('disconnect', () => {
+    if (socketPool[client.id]) {
+      let socketName = socketPool[client.id];
+      delete socketPool[client.id];
+    server.emit('broadcast', {
+      exitMessage: `${socketName} has left the chat.`,
+      userGroup: socketPool
+    })
+  }
+  });
+
+
     client.on("message", socket => {
     server.emit("broadcast", {
       name: socket.name,
       message: socket.message
     });
   });
+
 });
 
-let PORT = functions.config().server.port || 3000;
+let PORT = process.env.PORT || 3000;
 server.listen(PORT);
 console.log("Server listening on Port " + PORT);
-exports.app = functions.https.onRequest(server);
